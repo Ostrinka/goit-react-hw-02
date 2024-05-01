69023d6fed30183c291701aa9bfdf6b91fb3e174
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-05-01 15:46:43 +0300

    step one

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>Please leave your feedback about our service by selecting one of the options below.</p>
+      <Options setFeedback={setFeedback} />
+      <Feedback feedback={feedback} />
+    </div>
+  );
 }
 
-export default App
+
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,9 @@
+export default function Feedback({ feedback: { good, neutral, bad } }) {
+  return (
+    <ul>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+    </ul>
+  );
+}
