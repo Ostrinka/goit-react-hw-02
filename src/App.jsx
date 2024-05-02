57a9df8d64b439de8869a78416179c05731728b1
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-05-02 16:43:28 +0300

    step two

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,18 @@ import './App.css'
 export default function App() {
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
   return (
     <div>
       <h1>Sip Happens Café</h1>
       <p>Please leave your feedback about our service by selecting one of the options below.</p>
-      <Options setFeedback={setFeedback} />
+      <Options updateFeedback={updateFeedback} />
       <Feedback feedback={feedback} />
     </div>
   );
