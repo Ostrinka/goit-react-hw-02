56a18f0bff7bad2b8da01ee2abdec3a998b9e788
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-05-03 13:39:34 +0300

    step four

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,33 @@ export default function App() {
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
   const updateFeedback = (feedbackType) => {
-    setFeedback((prevFeedback) => ({
-      ...prevFeedback,
-      [feedbackType]: prevFeedback[feedbackType] + 1,
-    }));
-  };
+    if (feedbackType === 'reset') {
+      setFeedback({ good: 0, neutral: 0, bad: 0 });
+    } else {
+      setFeedback((prevFeedback) => ({
+        ...prevFeedback,
+        [feedbackType]: prevFeedback[feedbackType] + 1,
+      }));
+    }
+  }
 
-  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+    const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
-  return (
-    <div>
-      <h1>Sip Happens Café</h1>
-      <p>Please leave your feedback about our service by selecting one of the options below.</p>
-      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback}/>
-      {totalFeedback > 0 ? (
-        <Feedback
-          totalFeedback={totalFeedback}
-          feedback={feedback}
-        />
-      ) : (
-        <Notification message="No feedback yet" />
-      )}
-    </div>
-  );
-}
+    return (
+      <div>
+        <h1>Sip Happens Café</h1>
+        <p>Please leave your feedback about our service by selecting one of the options below.</p>
+        <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
+        {totalFeedback > 0 ? (
+          <Feedback
+            totalFeedback={totalFeedback}
+            feedback={feedback}
+          />
+        ) : (
+          <Notification message="No feedback yet" />
+        )}
+      </div>
+    );
+  }
 
 
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,13 @@
-export default function Options({ updateFeedback }) {
+export default function Options({ updateFeedback, totalFeedback }) {
   
   return (
     <div>
       <button onClick={() => updateFeedback('good')}>Good</button>
       <button onClick={() => updateFeedback('neutral')}>Neutral</button>
       <button onClick={() => updateFeedback('bad')}>Bad</button>
+      {totalFeedback > 0 && (
+        <button onClick={() => updateFeedback('reset')}>Reset</button>
+      )}
     </div>
   );
 }
